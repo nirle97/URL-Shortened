24523028ba090ac51c2caf7976686cff152a45e1
Nir Levy <nirle97@gmail.com>
2021-03-06 12:16:57 +0200

    statistics page is now connected to the jsonbin also

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const DataBase = require("../databaseClass")
-router.use(express.static("../public/statistics/statistics"));
-
+const { getDataJsonbin } = require("../backend-utils")
 router.get('/:shortUrl', async (req, res) => {
     const inputShortUrl = req.params.shortUrl;
-    await DataBase.getAllData()
+    DataBase.urls = await getDataJsonbin()
+    // await DataBase.getAllData()
     for (urlObj of DataBase.urls) {
         if (urlObj["shortUrl"] === inputShortUrl) {
-            res.send(JSON.stringify(urlObj, null, 2))
+            res.status(200).send(JSON.stringify(urlObj, null, 2))
             return;
         }
     }
-    res.send({"Error": `no such short URL: ${inputShortUrl}`})
+    res.status(404).send({"Error": `no such short URL: ${inputShortUrl}`})
 })
 
 module.exports = router;
